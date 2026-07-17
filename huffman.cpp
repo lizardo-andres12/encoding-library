c7f9e43b61593e752fb79be680dcf03411282344
lizardo-andres12 <lizardo.andres12@gmail.com>
2026-07-16 23:34:45 -0400

    Impl huffman code map

--- huffman.cpp
+++ huffman.cpp
@@ -1,14 +1,17 @@
 #include <algorithm>
 #include <bitset>
+#include <cassert>
 #include <fstream>
+#include <iostream>
 #include <memory>
-#include <print>
 #include <unordered_map>
 #include <utility>
 #include <vector>
 
 class huffman_code_map {
     struct tree_node;
+    struct code_metadata;
+
     using freq_node_pair_type = std::pair<uint32_t, std::unique_ptr<tree_node>>;
 
     static constexpr int MAX_BITS = 1 << 5;
@@ -31,9 +34,16 @@ public:
 	for (const char c : plain_file_contents) {
 	    ++char_counts[c];
 	}
+
 	std::vector<freq_node_pair_type> min_freq_heap = get_min_freq_heap(char_counts);
-	std::unique_ptr<tree_node> tree_root = get_huffman_tree_root(min_freq_heap);
-	return create_code_map_with_tree(min_freq_heap, tree_root.get(), char_counts.size());
+	std::unique_ptr<tree_node> tree_root = get_huffman_tree_root(std::move(min_freq_heap));
+	return create_code_map_with_tree(tree_root.get(), char_counts.size());
+    }
+
+    void debug() {
+	for (const auto& [c, meta] : code_map_) {
+	    std::cout << c << " : " << meta.encoding << " : " << meta.code_length << '\n';
+	}
     }
 
 private:
@@ -43,9 +53,14 @@ private:
 	char c = '\0';
     };
 
+    struct code_metadata {
+	std::bitset<MAX_BITS> encoding;
+	size_t code_length;
+    };
+
     explicit huffman_code_map(size_t n) : code_map_(n) {}
 
-    std::unordered_map<char, std::bitset<MAX_BITS>> code_map_;
+    std::unordered_map<char, code_metadata> code_map_;
 
     static std::vector<freq_node_pair_type>
     get_min_freq_heap(const std::unordered_map<char, uint32_t>& char_counts) {
@@ -61,7 +76,8 @@ private:
 	return min_freq_heap;
     }
 
-    static std::unique_ptr<tree_node> get_huffman_tree_root(std::vector<freq_node_pair_type>& min_freq_heap) {
+    static std::unique_ptr<tree_node>
+    get_huffman_tree_root(std::vector<freq_node_pair_type>&& min_freq_heap) {
 	while (min_freq_heap.size() > 1) {
 	    std::pop_heap(min_freq_heap.begin(), min_freq_heap.end(), MIN_HEAP_CMP);
 	    auto left_pair = std::move(min_freq_heap.back());
@@ -85,13 +101,14 @@ private:
     }
 	
     static huffman_code_map create_code_map_with_tree(
-	std::vector<freq_node_pair_type>& min_freq_heap, tree_node* tree_root, size_t initial_buckets = 16) {
-	huffman_code_map tree(initial_buckets);
+	tree_node* tree_root, size_t initial_buckets = 16) {
+	huffman_code_map code_map(initial_buckets);
 	std::bitset<MAX_BITS> path_state;
+
 	auto dfs = [&](
 		this const auto self, tree_node* const root, size_t bit_idx, huffman_code_map& hf) -> void {
 	    if (root->c != '\0') {
-		hf.code_map_[root->c] = path_state;
+		hf.code_map_[root->c] = {.encoding = path_state, .code_length = bit_idx + 1};
 		return;
 	    }
 
@@ -101,17 +118,28 @@ private:
 	    path_state.reset(bit_idx);
 	};
 
-	dfs(min_freq_heap.front().second.get(), 0uz, tree);
-	return tree;
+	dfs(tree_root, 0uz, code_map);
+	return code_map;
     }
 };
 
 int main() {
-    std::ifstream input_file("text.txt", std::ios::binary);
+    std::ifstream input_file("text.txt", std::ios::binary | std::ios::ate);
     if (!input_file.is_open()) {
-	std::print("Error opening file\n");
+	std::cerr << "Error opening file\n";
 	return 1;
     }
 
+    size_t size = input_file.tellg();
+    input_file.seekg(0);
+
+    std::string buffer;
+    buffer.resize(size);
+    input_file.read(buffer.data(), size);
+
+    huffman_code_map code = huffman_code_map::Create(buffer);
+    code.debug();
+
     return 0;
 }
+
